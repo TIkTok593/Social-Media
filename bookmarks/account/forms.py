1f73994ddf760b8c54520135e1f0ac5e9fec5000
abuYahya <58607882+TIkTok593@users.noreply.github.com>
2023-02-20 23:14:14 +0200

    Second commit

---
+++ bookmarks/account/forms.py
@@ -0,0 +1,6 @@
+from django import forms
+
+
+class LoginForm(forms.Form):
+    username = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput)  #This used to render the password html element

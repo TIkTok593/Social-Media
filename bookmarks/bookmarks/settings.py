f75abad95ade5b03a6750f1ef7e494142b6074ef
abuYahya <58607882+TIkTok593@users.noreply.github.com>
2023-03-11 07:36:04 +0200

    In Chapter 7 reach Building a generic activity stream application

--- bookmarks/bookmarks/settings.py
+++ bookmarks/bookmarks/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'social_django',
     'django_extensions',
     'images.apps.ImagesConfig',
-    # 'easy_thumbnails',
+    'easy_thumbnails',
 ]
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
